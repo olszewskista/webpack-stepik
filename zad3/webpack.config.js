aa546e585b29be52db2fcb2ac4518c48704286f5
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-29 14:22:10 +0100

    zad3

---
+++ zad3/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const imageminMozjpeg = require('imagemin-mozjpeg');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new ImageminPlugin({
+            disable: process.env.NODE_ENV !== 'production',
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            pngquant: {
+                quality: '75-90',
+            },
+            plugins: [
+                imageminMozjpeg({
+                    quality: 75,
+                    progressive: true,
+                }),
+            ],
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 3000,
+        hot: true,
+    },
+};
